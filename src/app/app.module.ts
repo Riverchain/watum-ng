ee7ec04deac610e3563111b67128d40df63107f7
Mostafa <ramezani.m@ut.ac.ir>
2020-03-05 22:55:10 +0330

    i am editing project in vscode now.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { AppCalculatorComponent } from './app-calculator/app-calculator.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ServerComponent,
+      AppCalculatorComponent,
+      CalculatorComponent
+   ],
+   imports: [
+      BrowserModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
