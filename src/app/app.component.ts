b7d4e265fba4821200face3f4b18b53db1dfdc93
Mostafa <ramezani.m@ut.ac.ir>
2020-03-08 10:38:06 +0330

    styling using css file component inline or outsource

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `
+  //   <app-calulator></app-calulator>
+  //   <app-calulator></app-calulator>
+  // `,
   styleUrls: ['./app.component.css']
+  // styles:['h3{background-color: dodgerblue}']
 })
 export class AppComponent {
   title = 'watum-ng-v002';
--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -4,6 +4,17 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.css']
+  // styles: [
+  //   `
+  //     p {
+  //       padding: 11px;
+  //       background-color: darkseagreen;
+  //       text-align: center;
+  //       font-size: 150%;
+  //       border-color: darkgreen;
+  //     }
+  //   `
+  // ]
 })
 export class CalculatorComponent implements OnInit {
 
